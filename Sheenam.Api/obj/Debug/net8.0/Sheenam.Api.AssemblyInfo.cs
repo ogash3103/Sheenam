2a893397f48b008c5411ed723837afde5e10a3ae
Og'abek Faxriddinov <114906301+ogash3103@users.noreply.github.com>
2025-04-26 21:51:07 +0500

    INFRA: Initislize API Project

--- Sheenam.Api/obj/Debug/net8.0/Sheenam.Api.AssemblyInfo.cs
+++ Sheenam.Api/obj/Debug/net8.0/Sheenam.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sheenam.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61135ad025dbe2b7ee3a1fb0581aa6f562a83af9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9eff28b6b14b46e75a4a8eceff213d01cab2f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sheenam.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sheenam.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
