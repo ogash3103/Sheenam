c2c52288bf58617e0c89bc9e27bfb519a6a1caf4
Og'abek Faxriddinov <114906301+ogash3103@users.noreply.github.com>
2025-04-30 12:57:18 +0500

    Resolved merge conflict in DeleteMe.cs

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,19 @@
+﻿/*Copyright(c) Coalition of Good-Hearted Engineers
+ * Free to Use Comfort and Peace
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+
+        public void ShouldBeTrue() => Assert.True(true);
+
+    }
+}
